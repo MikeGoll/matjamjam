8e480d8b6fece06566e54ffcccdd0f2cf9f29547
Michael Goll <mike_g97@hotmail.com>
2017-12-27 13:18:54 -0800

    Added player movement and rotation.

---
+++ matjamjam_unity/Assets/Scripts/MouseController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+	private float mouseX;
+     private float mouseY;
+     private float cameraDif;
+
+	private Vector3 _target;
+	public Camera playerCamera;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		cameraDif = playerCamera.transform.position.y - player.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseX = Input.mousePosition.x;
+        mouseY = Input.mousePosition.y;
+        _target = playerCamera.ScreenToWorldPoint(new Vector3(mouseX, mouseY, cameraDif));
+        Vector3 lookDirection = new Vector3 (_target.x, player.transform.position.y, _target.z); 
+        transform.LookAt(lookDirection);
+	}
+}
