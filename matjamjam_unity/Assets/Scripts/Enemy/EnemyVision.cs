545fcbb648bcf52f1e3546f895fb120c1abfb569
Uitiam <maitiumorton@gmail.com>
2017-12-27 16:09:04 -0800

    Added enemy vision and untilities

---
+++ matjamjam_unity/Assets/Scripts/Enemy/EnemyVision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour {
+	// Use this for initialization
+	private bool playerSpotted;
+	private float fieldOfView = 90.0f;
+	private SphereCollider col;
+	void Start () {
+		playerSpotted = false;
+		col = GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerStay(Collider other) {
+        Debug.Log(other.tag);
+		if(other.gameObject.tag == "Player"){
+			Debug.Log("Player in sphere");
+			checkVisionCone(other.gameObject);
+		}
+	}
+
+	public void checkVisionCone(GameObject player){
+
+		Vector3 direction = player.transform.position - transform.position;
+		float angle = Vector3.Angle(direction, transform.forward);
+
+		if(angle < fieldOfView * 0.5f){
+				if(Utilities.raycastWrap(transform.position, direction, col.radius, player)){
+					Debug.Log("Player Spotted");
+				}
+		}
+	}
+
+	public bool seesPlayer(){
+		return playerSpotted;
+	}
+}
---
+++ matjamjam_unity/Assets/Scripts/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour {
+
+	public static bool raycastWrap(Vector3 position, Vector3 direction, float distance, GameObject target) {
+		RaycastHit hit;
+		Debug.DrawRay(position, direction, Color.red);
+		if(Physics.Raycast(position, direction, out hit, distance)){
+			if(hit.collider.gameObject == target){
+				return true;
+			}
+		}
+		return false;
+	}
+}
